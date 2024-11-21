04da34cbc67781e8dc873d0f18ba428fdafa0923
Nathan Baleeta <nbaleeta@gmail.com>
2024-11-21 01:57:25 -0800

    Added bash script for building & uploading zookeeper image

---
+++ zookeeper/build_upload_image.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# https://www.redhat.com/en/blog/arguments-options-bash-scripts
+# https://medium.com/@max.kombarov/automating-bash-script-installation-and-docker-build-and-verification-in-ci-cd-by-qa-7210f536daf8
+# https://github.com/nathanbaleeta/magasin-docker-images/tree/main/zookeeper
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Build docker image for Magasin Zookeeper cluster coordinator"
+   echo
+   echo "Syntax: scriptTemplate [-r|t|v]"
+   echo "options:"
+   echo "r - Specify container registry."
+   echo "t - Specify docker image tag."
+   echo "v - Specify docker image version."
+   echo
+   echo "For example:"
+   echo 
+   echo "build_upload_image.sh -r myregistry.azurecr.io -t zookeeper -v 3.7.2"
+   echo
+}
+
+
+############################################################
+# Main program                                             #
+############################################################
+
+# Set variables
+REGISTRY=""
+TAG=""
+VERSION=""
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":r:t:v:" option; do
+   case $option in
+      r) # Enter container registry 
+         REGISTRY=$OPTARG;;
+      t) # Enter container tag 
+         TAG=$OPTARG;;
+      v) # Enter image version 
+         VERSION=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+Help
+
+############################################################
+# Validate the input options before building image         #
+############################################################
+if [[ ${REGISTRY} == "" ]]
+then
+  echo "Please enter the Container registry name string. For ex: ACR or dockerhub"
+  exit -1
+fi
+
+if [[ ${TAG} == "" ]]
+then
+  echo "Please enter the Container TAG name string. For ex: zookeeper"
+  exit -1
+fi
+
+
+if [[ ${VERSION} == "" ]]
+then
+  echo "Please enter the ZooKeeper VERSION string. For ex: 3.7.2"
+  exit -1
+fi
+
+############################################################
+# Build the Docker image                                   #
+############################################################
+
+docker buildx build --build-arg VERSION=${VERSION} \
+                    --platform linux/amd64 \
+                    -t ${REGISTRY}/${TAG}:${VERSION} \
+                    --push .
+
+
+
+
